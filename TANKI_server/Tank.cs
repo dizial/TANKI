ee546622bf4da4759aad298e2a9ecdec2b677c6e
MaltzevEvsevy <150817636+MaltzevEvsevy@users.noreply.github.com>
2023-12-01 09:23:35 +0500

    Add class Tank for Server
    Начал создание класс Tank

---
+++ TANKI_server/Tank.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TANKI_server
+{
+    internal class Tank
+    {
+        private int Health = 0;
+        private int Speed = 0;
+        private int[] Position = new int[2];
+        private int AngleOfRotation = 0;
+
+        public Tank()
+        {
+            Health = 100;
+            Speed = 10;
+            Position[0] = 0;
+            Position[1] = 0;
+            AngleOfRotation = 0;
+        }
+
+        public void SetHealth(int NewHealth)
+        {
+            this.Health = NewHealth;
+        }
+        public int GetHealth()
+        {
+            return this.Health;
+        }
+        public void SetSpeed(int NewSpeed)
+        {
+            this.Speed = NewSpeed;
+        }
+        public int GetSpeed()
+        {
+            return this.Speed;
+        }
+        public void SetAngleOfRotation(int NewAngleOfRotation)
+        {
+            this.AngleOfRotation = NewAngleOfRotation;
+        }
+        public int GetAngleOfRotation()
+        {
+            return this.AngleOfRotation;
+        }
+        public void SetPosition(int X, int Y)
+        {
+            this.Position[0] = X;
+            this.Position[1] = Y;
+        }
+        public int[] GetPosition()
+        {
+            return this.Position;
+        }
+    }
+}
