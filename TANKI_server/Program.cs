e16cdb0760750cc8b3826f8cb5e6d0cfa5bb18f9
MaltzevEvsevy <150817636+MaltzevEvsevy@users.noreply.github.com>
2023-12-07 10:13:57 +0500

    Work with communication
    Tried multithreaded TCP interaction

--- TANKI_server/Program.cs
+++ TANKI_server/Program.cs
@@ -6,6 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static System.Collections.Specialized.BitVector32;
+using System.Numerics;
+
+
 
 namespace TANKI_server
 {
@@ -16,76 +21,71 @@ namespace TANKI_server
             IPAddress ipAddr = new IPAddress(new byte[] { 127, 0, 0, 100 });  //iphost.addresslist[0];
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 8000);
 
-            // Создаем сокет Tcp/Ip
-            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+            Socket tcpListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try
             {
-                sListener.Bind(ipEndPoint);
-                sListener.Listen(10);
+                tcpListener.Bind(ipEndPoint);
+                tcpListener.Listen();    // запускаем сервер
+                Console.WriteLine("Сервер запущен. Ожидание подключений... ");
 
                 // Начинаем слушать соединения
                 while (true)
                 {
-                    Console.WriteLine("Ожидаем соединение через порт {0}", ipEndPoint);
+                    // получаем подключение в виде TcpClient
+                    //var tcpClient = await sListener.AcceptAsync();
+                    var tcpClient = tcpListener.AcceptAsync();
 
-                    // Программа приостанавливается, ожидая входящее соединение
-                    Socket handler = sListener.Accept();
-                    string data = null;
-
-                    // Мы дождались клиента, пытающегося с нами соединиться
-
-                    byte[] bytes = new byte[1024];
-                    int bytesRec = handler.Receive(bytes);
-
-                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
-
-                    string nomber = null;
-                    string action = null;
-                    string reply = null;
-
-                    // Показываем данные на консоли
-                    Console.Write("Полученный текст: " + data + "\n");
-
-                    string[] words = data.Split(' ');
-
-                    if (words.Length == 2)
-                    {
-                        nomber = words[0];
-                        action = words[1];
-                        Console.WriteLine($"{nomber} + {action}");
-                        Tank droid = new Tank();
-                        /*reply = "Спасибо за запрос в " + data.Length.ToString()
-                            + " символов\n" + droid.Tell();*/
-                    }
-                    else
-                    {
-                        reply = "Спасибо за запрос в " + data.Length.ToString()
-                            + " символов";
-                    }
-
-                    // Отправляем ответ клиенту\
-                    byte[] msg = Encoding.UTF8.GetBytes(reply);
-                    handler.Send(msg);
-
-                    if (data.IndexOf("<TheEnd>") > -1)
-                    {
-                        Console.WriteLine("Сервер завершил соединение с клиентом.");
-                        break;
-                    }
-
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
+                    // создаем новую задачу для обслуживания нового клиента
+                    Task.Run(async () => await ProcessClientAsync(tcpClient));
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
             }
             finally
             {
                 Console.ReadLine();
+            }
+
+            async Task ProcessClientAsync(Socket tcpClient)
+            {
+                string data = null;
+
+                byte[] bytes = new byte[1024];
+                int bytesRec = Convert.ToInt32(tcpClient.ReceiveAsync(bytes));
+
+                data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                // Показываем данные на консоли
+                Console.Write("Полученный текст: " + data + "\n");
+
+                string nomber = null;
+                string action = null;
+                string reply = null;
+
+                string[] words = data.Split(' ');
+
+                if (words.Length == 2)
+                {
+                    nomber = words[0];
+                    action = words[1];
+                    Console.WriteLine($"{nomber} + {action}");
+                    Tank droid = new Tank();
+                    reply = "Спасибо за запрос в " + data.Length.ToString()
+                        + " символов\n" + "";
+                }
+                else
+                {
+                    reply = "Спасибо за запрос в " + data.Length.ToString()
+                        + " символов";
+                }
+
+                // Отправляем ответ клиенту\
+                byte[] msg = Encoding.UTF8.GetBytes(reply);
+                await tcpClient.SendAsync(msg);
+
+                tcpClient.Shutdown(SocketShutdown.Both);
+                tcpClient.Close();
             }
         }
     }
